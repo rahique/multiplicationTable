5622971cf52208038f806b42b7c1ce080e49ee3b
rahique <rahique@Rahique>
2023-08-05 22:05:47 +0600

    first commit

---
+++ mt.js
@@ -0,0 +1,23 @@
+function hello() {
+  let dtable = document.querySelector('.table');
+  let input = document.querySelector('.input').value;
+  let arr = [];
+  let i = 1;
+  for (i; i <= input; i++) {
+    arr.push(i);
+  }
+  arr.forEach((x) => {
+    for (let i = 1; i !== 11; i++) {
+      dtable.innerHTML += `<h1 style="text-align: center">${x} X ${i} = ${
+        x * i
+      }<br><h1>`;
+      // dtable.appendChild(table);
+      if (i === 10) {
+        dtable.innerHTML += '<hr>';
+      }
+    }
+  });
+}
+function bye() {
+  location.reload();
+}
