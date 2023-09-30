085cbba8bb7be786127ad654760609b418430c19
rahique <rahique@Rahique>
2023-09-30 21:06:26 +0600

    new

--- mt.js
+++ mt.js
@@ -1,9 +1,14 @@
+let helloBtn = document.querySelector('.hello');
+let byeBtn = document.querySelector('.bye');
+
+helloBtn.addEventListener('click', hello);
+byeBtn.addEventListener('click', bye);
+
 function hello() {
   let dtable = document.querySelector('.table');
   let input = document.querySelector('.input').value;
   let arr = [];
-  let i = 1;
-  for (i; i <= input; i++) {
+  for (let i = 1; i <= input; i++) {
     arr.push(i);
   }
   if (input !== 0 || '') {
@@ -14,7 +19,6 @@ function hello() {
       dtable.innerHTML += `<h1 style="text-align: center">${x} X ${i} = ${
         x * i
       }<br><h1>`;
-      // dtable.appendChild(table);
       if (i === 10) {
         dtable.innerHTML += '<hr>';
       }
